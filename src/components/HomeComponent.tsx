0198ecd11ef5b09b1eee03f4ebf61aad99fd2f3d
Tedo <tedojanelidze7@gmail.com>
2023-07-22 19:28:34 +0400

    created home page

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import { planetDataInterface } from '../interface/planetDataInterface';
 import { Link } from 'react-router-dom';
